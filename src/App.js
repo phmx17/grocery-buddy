7f03810a5f61dec9002adfa59693b4b843b54d73
Slim Yelow <slimyelow@gmail.com>
2021-06-23 17:22:29 +0200

    add first elements in place

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''})
+  // handler functions
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('form has been submitted');
+  }
+
+  return <section className="section-center">
+    <form className="grocery-form" onSubmit={handleSubmit}>
+      {alert.show && <Alert />}
+      <button  type="submit" className="btn" >Submit</button>
+
+    </form>
+    <div className="grocery-container">
+      <List />
+      <button className="clear-btn">clear items</button>
+    </div>
+  </section>
 }
 
 export default App
+ 
